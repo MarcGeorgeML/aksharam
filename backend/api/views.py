c0d91e936478fa4f1ba0147640a147ac7589520a
tims-exe <timonkjohn@gmail.com>
2025-02-19 11:17:44 +0530

    backend cors setup

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def testhtml(request):
+    html_content = """<div style='font-size:20px; color:blue;'>Hello, World!</div>"""
+    return HttpResponse(html_content)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-z--*-*tqqsjo@a)h9ey^x_munsc7x@si+@*4&8wgjr304wzh=v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+    "http://127.0.0.1:5500",  
 ]
 
 ROOT_URLCONF = 'backend.urls'
