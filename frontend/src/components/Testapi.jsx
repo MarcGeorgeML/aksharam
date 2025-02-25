c0991da0c4a7c97d988e38abfb94041c0553d85b
tims-exe <timonkjohn@gmail.com>
2025-02-25 20:12:52 +0530

    setup tailwind

--- frontend/src/components/Testapi.jsx
+++ frontend/src/components/Testapi.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Testapi = () => {
     const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+    //const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("http://127.0.0.1:8000/api/test/")
@@ -13,12 +13,14 @@ const Testapi = () => {
                 return response.json();
             })
             .then(data => setData(data.message))
-            .catch(error => setError(error.message));
+            .catch(error => console(error));
     }, []);
 
     return (
         <div>
-            {error ? <p>Error: {error}</p> : <p>{data ? data : "Loading..."}</p>}
+            <h1 className="text-3xl font-bold underline">
+                { data }
+            </h1>
         </div>
     );
 }
