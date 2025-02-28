4ec759f410817752a815348a1d3b75de13fc8135
tims-exe <timonkjohn@gmail.com>
2025-02-28 19:16:23 +0530

    page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Testapi from './components/Testapi'
 
 function App() {
   return (
-    <div>
-      <Testapi/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/test' element={<Testapi/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
